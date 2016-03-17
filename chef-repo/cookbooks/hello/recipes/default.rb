0640c26eae7f346563040716c05763e218e0ddeb
root <root@wordpress.(none)>
2016-03-17 10:31:08 +0900

    up chef-data

---
+++ chef-repo/cookbooks/hello/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: hello
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+execute "ls" do
+	command "ls"
+	#action :nothing
+end
