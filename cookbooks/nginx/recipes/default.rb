192ece386cd727e06894ca644518b4014e0b8125
yhidetoshi <hyajima.1014@gmail.com>
2016-03-19 03:18:03 +0900

    add nginx config of wordpress

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,3 +1,14 @@
 package "nginx" do
   action :install
 end
+
+template "/etc/nginx/conf.d/wp.conf" do
+  source "wordpress.conf.erb"
+  owner  "root"
+  group  "root"
+  mode  0644
+  variables({
+	:hostname => `/bin/hostname`.chomp
+  })
+end
+
