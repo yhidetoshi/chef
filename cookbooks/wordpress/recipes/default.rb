5b8c7d22dcb0dbc1c01e903521edcaf7286400f5
yhidetoshi <hyajima.1014@gmail.com>
2016-03-19 17:11:04 +0900

    add wordpress recipe

--- cookbooks/wordpress/recipes/default.rb
+++ cookbooks/wordpress/recipes/default.rb
@@ -1,8 +1,21 @@
-execute "get-file-wordpress" do
-    user "root" 
-    command <<-EOH 
-	git clone #{node['file']['wordpress']}
-        mv wordpress /var/www/
-    EOH
+#execute "get-file-wordpress" do
+#    user "root" 
+#    command <<-EOH 
+#	git clone #{node['file']['wordpress']}
+#        mv wordpress /var/www/
+#    EOH
     #command "wget -P /var/www/ https://github.com/yhidetoshi/wordpress"
+#end
+
+install_packages = %w[
+	php php-mbstring php-mysql php-fpm mysql-server
+]
+install_packages.each do |pkg|
+  bash "install_#{pkg}" do
+	user "root"
+  	code <<-EOC
+	  yum clean all
+          yum -y install #{pkg}
+        EOC
+   end
 end
