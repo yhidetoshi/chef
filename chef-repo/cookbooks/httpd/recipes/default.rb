a57f067586bfbf061715309b2cf816545beaf87b
yajima <hyajima.jaist.job@gmail.com>
2016-03-22 20:39:42 +0900

    Update default.rb

--- chef-repo/cookbooks/httpd/recipes/default.rb
+++ chef-repo/cookbooks/httpd/recipes/default.rb
@@ -1,1 +1,11 @@
 httpd_install
+template "/etc/httpd/conf.d/mysite.conf" do
+  source "mysite.conf.erb"
+  owner	 "root"
+  group  "root"
+  mode   0644
+  action :create
+  variables({
+	:hostname => `/bin/hostname`.chomp
+  })
+end
